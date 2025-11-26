404635d539cc85f410461e6dd8b10aa9a78a63c5
Ansh Pancholi <pancholiansh17@gmail.com>
2025-11-26 11:28:48 +0000

    rerender

--- app/src/app/api/links/route.ts
+++ app/src/app/api/links/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 import { nanoid } from 'nanoid';
 
+export const dynamic = 'force-dynamic';
+
 const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest) {
